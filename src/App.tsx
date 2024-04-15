6b61d46219cc32fb501a70b9b1589e2361d58696
lassev05 <vestergaardlasse2@gmail.com>
2024-04-15 12:11:44 +0200

    setup map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 export default function App() {
   return (
-    <div className="text-green-500">App</div>
+    <main className="container mx-auto pt-10">
+      <Link to={"/map"} className="bg-lightGreen px-8 py-3 rounded-md text-white font-bold">
+        Go to map
+      </Link>
+    </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+
+// Pages
+import App from './App.tsx'
+import Map from './pages/Map.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/map",
+    element: <Map />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
