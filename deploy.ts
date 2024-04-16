d01c74cf42927cafe07e9e6acab0efa91f83cb48
lasse <77295879+lassejlv@users.noreply.github.com>
2024-04-16 19:01:44 +0200

    Add deploy script

---
+++ deploy.ts
@@ -0,0 +1,15 @@
+import { $ } from "bun";
+import ora from "ora";
+
+const buildSpinner = ora("Building site...").start();
+
+await $`bunx vite build`.quiet();
+
+buildSpinner.succeed("Site built!");
+
+const deploySpinner = ora("Deploying site...").start();
+
+await $`netlify deploy --prod --dir=dist`.quiet();
+
+deploySpinner.succeed("Site has been deployed!");
+console.log("https://pollentals-app.netlify.app");
